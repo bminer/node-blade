fddae9571914588befa33ae83b2bd555f630a6b1
Blake Miner <miner.blake@gmail.com>
2012-07-13 18:36:33 +0000

    * Added element preservation capabilities to the Live UI plugin (issue #55) * Element preservation causes IE to crash; need to fix

--- plugins/liveui.js
+++ plugins/liveui.js
@@ -9,12 +9,17 @@
 	Adds the following to the `blade` global variable:
 		- Model
 		- Context
+	Adds the following to `blade.runtime`:
+		- render(viewName, locals, cb)
+		- renderTo(element, viewName, locals [, cb])
+	Adds the following functions to jQuery.fn:
+		- render(viewName, locals [, cb])
 	
 	Browser Support:
 		-Chrome
 		-Firefox 4+ (not tested)
-		-IE 9+
-		-IE 8 (requires definePropertyIE8 plugin; IE is dumb)
+		-IE 9+ - NOT WORKING!!!  Causes browser to freeze (probably a resursive loop).
+		-IE 8 - (not tested - requires definePropertyIE8 plugin; IE is dumb)
 		-Safari 5.1+ (not tested)
 		-Opera 12+ (not tested)
 	If using the definePropertyIE8 plugin, include it into your HTML document
@@ -201,4 +206,151 @@
 	Model.prototype.serialize = function() {
 		return JSON.stringify(this._rawData);
 	};
+	
+	/* render(viewName, locals, cb)
+		Asynchronously loads (if necessary) and renders the specified template
+		using the specified locals in a new Context. If the Context is invalidated,
+		the template will be re-rendered and the callback will be called again.
+		- viewName - the name of the view to be loaded and rendered
+		- locals - the locals to be passed to the view. If a `Model` object is
+			passed to this method, the Model's `observable` Object will be passed
+			to the view.
+		- cb - a callback of the form cb(err, html) where `html` is an string of
+			HTML produced by the view template
+	*/
+	blade.runtime.render = function(viewName, locals, cb) {
+		//Load and render the template
+		blade.runtime.loadTemplate(viewName, function(err, tmpl) {
+			if(err) return cb(err);
+			(function renderTemplate() {
+				function renderIt() {
+					tmpl(locals ? locals.observable || locals : {}, cb);
+				}
+				var context = new Context();
+				context.on_invalidate(renderTemplate); //recurse
+				context.run(renderIt);
+			})();
+		});
+	};
+	/* renderTo(element, viewName, locals [, cb])
+		Same as render(), except the output of the view is immediately injected
+		into the specified element.  In addition, any event handlers created by
+		the view are bound.  Finally, the element in focus is "preserved" if jQuery
+		is available and if the element either has an 'id' attribute or has a 'name'
+		attribute and a parent who has an 'id' attribute.
+		Also, from within the callback, `this` refers to the `element`.
+	*/
+	blade.runtime.renderTo = function(el, viewName, locals, cb) {
+		blade.runtime.render(viewName, locals, function(err, html, info) {
+			if(err) {if(cb) cb.call(el, err); return;}
+			try
+			{
+				//Start preserving the element in focus, if necessary
+				var focus = document.activeElement,
+					$ = jQuery,
+					preserve = jQuery && //jQuery is required
+						//if <body> is in focus, ignore preservation
+						! $(focus).is("body") &&
+						//the element must have an 'id' or a 'name' and a parent with an 'id'
+						(focus.id || (focus.parentNode.id && focus.name) ) &&
+						//Make sure that this node is a descendant of `el`
+						$(focus).parents().index(el) >= 0;
+				if(preserve)
+				{
+					//Save the ID and value of this element
+					var id = focus.id, oldValue, tmpValue,
+						newElementQuery = id ? "#" + id :
+							"#" + focus.parentNode.id + " > [name=" + focus.name + "]";
+					//Save the selection, if needed
+					if($(focus).is("input[type=text],input[type=password],textarea"))
+					{
+						var selectionStart = focus.selectionStart,
+							selectionEnd = focus.selectionEnd;
+					}
+					//Remove event handlers and attributes; in Chrome, 'blur' and possibly 'change'
+					//events are fired when an in-focus element is removed from the DOM
+					while(focus.attributes.length > 0)
+						focus.removeAttributeNode(focus.attributes.item(0) );
+					//Add a "change" event handler. When in-focus elements are removed from,
+					//the DOM, 'blur' or 'change' might be triggered.
+					focus.onchange = function(e) {
+						tmpValue = focus.value;
+						return false; //prevent the change from occurring
+					};
+					//Remove all children and any leftover event handlers
+					//(although blur might still fire if the event handler could not be removed)
+					focus = focus.cloneNode();
+				}
+				//Insert newly rendered content (jQuery is not required here)
+				if(el.html)
+					el.html(html);
+				else
+					el.innerHTML = html;
+				//Preserve element focus, cursor position, etc.
+				if(preserve)
+				{
+					//Find new element in newly rendered content
+					var newElement = $(newElementQuery);
+					//If found, do element preservation stuff...
+					if(newElement.length == 1)
+					{
+						//Replace newElement
+						newElement.replaceWith(focus);
+						newElement = newElement[0];
+						oldValue = newElement.value;
+						//Add new child nodes and attributes
+						while(newElement.childNodes.length > 0)
+						{
+							var child = newElement.childNodes.item(i);
+							newElement.removeChild(child);
+							focus.appendChild(child);
+						}
+						while(newElement.attributes.length > 0)
+						{
+							var attr = newElement.attributes.item(i);
+							newElement.removeAttributeNode(attr);
+							focus.setAttributeNode(attr);
+						}
+						//Set value if a change event was previously triggered
+						if(tmpValue)
+						{
+							focus.value = tmpValue;
+							//Setup a one-time blur event handler to ensure
+							//that a 'change' event is triggered
+							$(focus).blur(function(e) {
+								if(this.value !== oldValue)
+									$(this).trigger('change');
+								$(this).unbind(e);
+							});
+						}
+						//Set focus and cursor
+						focus.focus();
+						if($(focus).is("input[type=text],input[type=password],textarea"))
+						{
+							focus.selectionStart = selectionStart;
+							focus.selectionEnd = selectionEnd;
+						}
+					}
+				}
+				//Register event handlers
+				for(var i in info.eventHandlers)
+				{
+					var events = info.eventHandlers[i].events.split(" "),
+						elem = document.getElementById(i);
+					for(var j = 0; j < events.length; j++)
+						elem['on' + events[j]] = info.eventHandlers[i].handler;
+					//Delete comment before element
+					elem.parentNode.removeChild(elem.previousSibling);
+				}
+				if(cb) cb.call(el, null, html, info);
+			}
+			catch(e) {if(cb) cb.call(el, e);}
+		});
+	};
+
+	if(jQuery)
+		jQuery.fn.render = function(viewName, locals, cb) {
+			blade.runtime.renderTo(this, viewName, locals, cb);
+		};
+
 })();
