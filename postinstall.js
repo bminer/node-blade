7a966867ad27b0bd9d353c072d4c21d86f773625
Blake Miner <miner.blake@gmail.com>
2013-04-08 04:55:45 +0000

    Push to 3.2.2 Moved Meteor smart package to another repository (https://github.com/bminer/meteor-blade) Removed postinstall script entirely

--- postinstall.js
+++
@@ -1,9 +0,0 @@
-/* Installs symlinks in ./meteor smart package directory since Meteor does not ship
-with Blade just yet...
-I'm hoping that I can get rid of this script someday... */
-var fs = require('fs');
-if(!fs.existsSync)
-	fs.existsSync = require('path').existsSync; //Node 0.6 compatibility
-//bash: [ -h ./meteor/runtime.js ] || ln -s ../lib/runtime.js ./meteor/runtime.js
-if(!fs.existsSync("./meteor/runtime.js") )
-	fs.symlink("../lib/runtime.js", "./meteor/runtime.js");
